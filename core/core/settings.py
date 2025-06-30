96c5238af2dbdc8e1ca0e65d7edd3435adadbb1d
sma7495 <aut.aseyedali@gmail.com>
2025-06-30 18:45:44 +0330

    The environment variables, SECRET_KEY and DEBUG was set by python-decouple

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4*i*$kwd@8=2^_cd@)sf-qk%_puxu-gemy+%tdh=jf)i^cmnsv'
+# SECRET_KEY = 'django-insecure-4*i*$kwd@8=2^_cd@)sf-qk%_puxu-gemy+%tdh=jf)i^cmnsv'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default = False, cast = bool)
 
 ALLOWED_HOSTS = []
 
