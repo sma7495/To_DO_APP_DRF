4b2a5ddee7e49981695c3911cd221a3e7f665691
sma7495 <aut.aseyedali@gmail.com>
2025-07-04 17:12:49 +0330

    some changes

--- core/core/settings.py
+++ core/core/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'mail_templated',
-    'django_cleanup.apps.CleanupConfig',
     
     'account',
     'task',
@@ -184,17 +183,3 @@ SPECTACULAR_SETTINGS = {
     'REDOC_DIST': 'SIDECAR',
 
 }
-
-
-
-# Static files :
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-# Media Settings:
-MEDIA_URL = '/media/' # base url for accessing to media files
-
-MEDIA_ROOT = BASE_DIR / 'media' # path for saving media files on server
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
 from rest_framework import permissions
 # from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
-from . import settings
 
 
 # schema_view = get_schema_view(
@@ -58,7 +55,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('task/', include("task.urls")),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
